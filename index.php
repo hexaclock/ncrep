1dc537e8cba405883289777d17182441e32019e8
daniel <daniel@armani.vinakovsky.com>
2015-04-23 22:13:55 -0400

    pull test on webserver

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <body>
 <?php
 echo "ncrep..<br />";
-echo "anything that gets pushed to github repo will automatically get pulled here.<br>-DanV";
+echo "anything that gets pushed to github repo will automatically get pulled here.<br>";
+echo "test 4/23/2015 ~ April 23, 2015 - Dan V";
 ?>
 </body>
 </html>
