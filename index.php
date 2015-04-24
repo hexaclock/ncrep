8cdaf5f30c5ae4da437c65aec459028afe58caf2
daniel <daniel@armani.vinakovsky.com>
2015-04-23 22:47:45 -0400

    removed extra info from index.php

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <head></head>
 <body>
 <?php
-echo "ncrep..<br />";
-echo "anything that gets pushed to github repo will automatically get pulled here.<br>";
-echo "test 4/23/2015 ~ April 23, 2015 - Dan V";
+echo "ncrep";
 ?>
 </body>
 </html>
