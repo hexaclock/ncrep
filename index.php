f5fd7fbd79978e5902aef407f9b670d5aa106ea5
seanloveall <sloveall@stevens.edu>
2015-04-24 00:40:48 -0400

    committing index

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 				{
 					$target_dir = 'temp/';
 					$target_file = $target_dir.sha1_file($_FILES["packet"]["tmp_name"]); //basename($_FILES["packet"]["name"]);
-					$filetype = pathinfo($target_file,PATHINFO_EXTENSION);
+					$filetype = pathinfo($_FILES["packet"]["name"],PATHINFO_EXTENSION);
 					
 					if(file_exists($target_file)) //if file exists then fail
 						echo "File already exists.";
