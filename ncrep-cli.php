63579a53ca1f23bcd0fe960edcae2c5c2e80a439
daniel <daniel@armani.vinakovsky.com>
2015-04-24 10:38:57 -0400

    added basic cli interface

---
+++ ncrep-cli.php
@@ -0,0 +1,26 @@
+<?php
+
+require("classes/Statistics.php");
+
+if ($argc < 2)
+{
+	print "usage: php $argv[0] <list of CSV packet capture files>\n";
+	return 1;
+}
+
+for ($i=1; $i<$argc; $i++)
+{
+	$statsobj = new Statistics($argv[$i]);
+	$res      = $statsobj->getCredentials();
+	//$pcnt     = $statsobj->getProtocolsCount();
+	$totcnt   = $statsobj->getTotalPacketsCount();
+	print "\nStatistics for file: $argv[$i]:\n";
+	print("The total number of packets read from the file: $totcnt\n");
+	print_r($res);
+	print_r($statsobj->getProtocolsPercent());
+	//$ipcnt    = $statsobj->getTotalCountForIP("192.168.2.20");
+}
+
+return 0;
+
+?>
