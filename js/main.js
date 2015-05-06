7f58999be95b2012eced5a6246594f8a04e0d7f3
seanloveall <sloveall@stevens.edu>
2015-05-06 08:25:56 -0400

    index changes + JS changes + css changes

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(function() {
 function toggle(e)
 {
 	$(".tabbed span.highlight").removeClass("highlight");
-	if($("."+e.data.div).css("display" == 'none')
+	if($("."+e.data.div).css("display" == 'none'))
 	{
 		$("."+e.data.div).show();
 		$("."+e.data.butt).addClass("highlight");
@@ -35,49 +35,6 @@ function toggle(e)
 	else
 	{
 		$("."+e.data.div).hide();
-	}
-}
-
-function toggle_actualstats()
-{
-	resetHighlight();
-	$(".togglestats").parent().find("div").each(function() {
-		if(!$(this).hasClass("actualstats"))
-			$(this).hide();
-	});
-	if($(".actualstats").css("display") == 'none')
-	{
-		$(".actualstats").show();
-		$(".togglestats").addClass("highlight");
-	}
-	else
-	{
-		$(".actualstats").hide();
-		$(".togglestats").removeClass("highlight");
-	}
-}
-
-function resetHighlight()
-{
-	$(".tabbed span.highlight").removeClass("highlight");
-}
-
-function toggle_rawdata()
-{
-	resetHighlight();
-	$(".toggleraw").parent().find("div").each(function() {
-		if(!$(this).hasClass("rawdata"))
-			$(this).hide();
-	});
-	if($(".rawdata").css("display") == 'none')
-	{
-		$(".rawdata").show();
-		$(".toggleraw").addClass("highlight");
-	}
-	else
-	{
-		$(".rawdata").hide();
-		$(".toggleraw").removeClass("highlight");
 	}
 }
 
